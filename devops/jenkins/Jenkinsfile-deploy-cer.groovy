65ec12b15c41cd5cc238893cc1837c4551161698
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2025-02-02 16:07:29 -0500

    Update groovys

--- devops/jenkins/Jenkinsfile-deploy-cer.groovy
+++ devops/jenkins/Jenkinsfile-deploy-cer.groovy
@@ -17,65 +17,64 @@ pipeline {
         GIT_CREDENTIALS = credentials('PATH_Jenkins')
         GIT_COMMITTER_NAME = 'josephmn'
         GIT_COMMITTER_EMAIL = 'josephcarlos.jcmn@gmail.com'
-        GIT_BRANCH = 'develop'
-    }
-
-    parameters {
-        string(name: 'NEW_VERSION', defaultValue: '1.0.0', description: 'Nueva version para el release (ej. 1.0.0)')
-        booleanParam(name: 'DOCKER', defaultValue: true, description: 'Desplegar y Ejecutar APP en DOCKER?')
-        booleanParam(name: 'NOTIFICATION', defaultValue: false, description: 'Deseas notificar por correo?')
-        string(name: 'CORREO', defaultValue: 'josephcarlos.jcmn@gmail.com', description: 'Correo para notificaciones')
+        NEW_VERSION = ''
     }
 
     stages {
-        stage('Checkout Code') {
+        stage('Compile Repository') {
             steps {
-                echo "######################## : ======> Clonando codigo desde la rama release..."
                 script {
-                    bat """
-                        if exist .git (
-                            echo "Repositorio ya clonado, ejecutando git pull..."
-                            git reset --hard
-                            git pull origin develop
-                        ) else (
-                            echo "Repositorio no encontrado, clonando..."
-                            git clone -b ${GIT_BRANCH} https://${env.GIT_CREDENTIALS_USR}:${env.GIT_CREDENTIALS_PSW}@github.com/josephmn/config-server.git .
-                        )
-                    """
+                    if (params.NOTIFICATION) {
+                        notifyJob('JOB Jenkins', params.CORREO)
+                    }
+                    echo "######################## : ======> EJECUTANDO COMPILE..."
+                    // Usar 'bat' para ejecutar comandos en Windows, para Linux usar 'sh'
+                    bat 'mvn clean compile'
                 }
             }
         }
 
         stage('Delete Release Branch') {
             steps {
-                echo "######################## : ======> Delete Release Branch: release/${params.NEW_VERSION}..."
+                echo "######################## : ======> ELIMINANDO RAMA RELEASE..."
                 script {
+                    // Obtener la versión en Windows usando un archivo temporal
+                    bat '''
+                        mvn help:evaluate -Dexpression=project.version -q -DforceStdout > version.txt
+                    '''
+                    def version = readFile('version.txt').trim()
+                    // Remover -SNAPSHOT si existe
+                    version = version.replaceAll("-SNAPSHOT", "")
+                    NEW_VERSION = version
+
+                    echo "======> Eliminando rama release: release/${version}..."
+
                     // Verificar si la rama local existe
                     def localBranchExists = bat(
-                            script: """git show-ref refs/heads/release/${params.NEW_VERSION}""",
+                            script: """git show-ref refs/heads/release/${version}""",
                             returnStatus: true
                     ) == 0
 
                     if (localBranchExists) {
                         echo "=========> La rama existe en local, procediendo a eliminarla..."
-                        bat "git branch -d release/${params.NEW_VERSION}"
-                        echo "=========> Rama local 'release/${params.NEW_VERSION}' eliminada."
+                        bat "git branch -d release/${version}"
+                        echo "=========> Rama local 'release/${version}' eliminada."
                     } else {
-                        echo "=========> La rama local 'release/${params.NEW_VERSION}' no existe."
+                        echo "=========> La rama local 'release/${version}' no existe."
                     }
 
                     // Verificar si la rama remota existe
                     def branchExists = bat(
-                            script: "git show-ref --verify --quiet refs/remotes/origin/release/${params.NEW_VERSION}",
+                            script: "git show-ref --verify --quiet refs/remotes/origin/release/${version}",
                             returnStatus: true
                     ) == 0
 
                     if (branchExists) {
                         echo "=========> La rama existe en remoto, procediendo a eliminarla..."
-                        bat "git push origin --delete release/${params.NEW_VERSION}"
-                        echo "=========> Rama remota release/${params.NEW_VERSION} eliminada"
+                        bat "git push origin --delete release/${version}"
+                        echo "=========> Rama remota release/${version} eliminada"
                     } else {
-                        echo "=========> La rama remota 'release/${params.NEW_VERSION}' no existe."
+                        echo "=========> La rama remota 'release/${version}' no existe."
                     }
                 }
             }
@@ -83,19 +82,19 @@ pipeline {
 
         stage('Create Release Branch') {
             steps {
-                echo "######################## : ======> Create Release Branch..."
+                echo "######################## : ======> CREANDO RAMA PARA LA RELEASE..."
                 script {
                     bat """
                         git config user.email "${GIT_COMMITTER_EMAIL}"
                         git config user.name "${GIT_COMMITTER_NAME}"
 
                         echo "=========> Creando rama de release..."
-                        git checkout -b release/${params.NEW_VERSION}
-                        git push origin release/${params.NEW_VERSION}
+                        git checkout -b release/${NEW_VERSION}
+                        git push origin release/${NEW_VERSION}
                     """
 
                     bat """
-                        mvn versions:set -DnewVersion=${params.NEW_VERSION}
+                        mvn versions:set -DnewVersion=${NEW_VERSION}
                     """
                     bat """
                         mvn versions:commit
@@ -103,46 +102,46 @@ pipeline {
 
                     bat """
                         git add .
-                        git commit -m "RC version ${params.NEW_VERSION}"
-                        git push origin release/${params.NEW_VERSION}
+                        git commit -m "RC version ${NEW_VERSION}"
+                        git push origin release/${NEW_VERSION}
                     """
                 }
             }
         }
 
-        stage('Delete Last Tag Release Candidate') {
-            steps {
-                echo "######################## : ======> Eliminando el ultimo tag que contiene '${params.NEW_VERSION}'..."
-                script {
-                    def lastTag = bat(
-                            script: "@git tag -l \"*${params.NEW_VERSION}*\" --sort=-v:refname",
-                            returnStdout: true
-                    ).trim()
-
-                    if (lastTag?.trim()) {  // Validamos que lastTag no sea null y no esté vacío
-                        echo "=========> Ultimo tag encontrado: ${lastTag}"
-
-                        try {
-                            // Borrar el tag localmente
-                            bat "git tag -d ${lastTag}"
-                            echo "=========> Tag eliminado localmente"
-
-                            // Borrar el tag en el repositorio remoto
-                            bat "git push origin --delete ${lastTag}"
-                            echo "=========> Tag eliminado del repositorio remoto"
-                        } catch (Exception e) {
-                            error "Error al eliminar el tag: ${e.getMessage()}"
-                        }
-                    } else {
-                        echo "=========> No se encontro ningun tag con version: RC-${params.NEW_VERSION}-CERT-*"
-                    }
-                }
-            }
-        }
+//        stage('Delete Last Tag Release Candidate') {
+//            steps {
+//                echo "######################## : ======> ELIMINANDO EL ULTIMO TAG QUE CONTIENE LA NUEVA VERSION: '${NEW_VERSION}'..."
+//                script {
+//                    def lastTag = bat(
+//                            script: "@git tag -l \"*${NEW_VERSION}*\" --sort=-v:refname",
+//                            returnStdout: true
+//                    ).trim()
+//
+//                    if (lastTag?.trim()) {  // Validamos que lastTag no sea null y no esté vacío
+//                        echo "=========> Ultimo tag encontrado: ${lastTag}"
+//
+//                        try {
+//                            // Borrar el tag localmente
+//                            bat "git tag -d ${lastTag}"
+//                            echo "=========> Tag eliminado localmente"
+//
+//                            // Borrar el tag en el repositorio remoto
+//                            bat "git push origin --delete ${lastTag}"
+//                            echo "=========> Tag eliminado del repositorio remoto"
+//                        } catch (Exception e) {
+//                            error "Error al eliminar el tag: ${e.getMessage()}"
+//                        }
+//                    } else {
+//                        echo "=========> No se encontro ningun tag con version: RC-${NEW_VERSION}-CERT-*"
+//                    }
+//                }
+//            }
+//        }
 
         stage('Create Release Candidate') {
             steps {
-                echo "######################## : ======> Generando Release Candidate (RC-${params.NEW_VERSION}-CERT-*)..."
+                echo "######################## : ======> GENERANDO RELEASE CANDIDATE (RC-${NEW_VERSION}-CERT-*)..."
                 script {
                     bat """
                         git config user.email "${GIT_COMMITTER_EMAIL}"
@@ -153,8 +152,8 @@ pipeline {
                     """
 
                     bat """
-                        echo "=========> Cambiando a rama release/${params.NEW_VERSION}..."
-                        git checkout release/${params.NEW_VERSION}
+                        echo "=========> Cambiando a rama release/${NEW_VERSION}..."
+                        git checkout release/${NEW_VERSION}
                     """
 
                     bat """
@@ -163,7 +162,7 @@ pipeline {
                         for /f "tokens=2 delims==" %%a in ('wmic os get localdatetime /value ^| find "="') do set datetime=%%a
                         set today=%datetime:~0,8%
                         set time=%datetime:~8,4%
-                        set tagName=RC-${params.NEW_VERSION}-CERT-%today%%time%
+                        set tagName=RC-${NEW_VERSION}-CERT-%today%%time%
 
                         echo "=========> tagName: %tagName%"
                         git tag %tagName%
@@ -174,9 +173,12 @@ pipeline {
         }
 
         stage('Creating Network for Docker') {
+            when {
+                expression { params.DOCKER }
+            }
             steps {
+                echo "######################## : ======> EJECUTANDO CREACION DE RED PARA DOCKER: ${NETWORK}..."
                 script {
-                    echo "######################## : ======> EJECUTANDO CREACIÓN DE RED PARA DOCKER..."
                     def networkExists = bat(
                             script: "docker network ls | findstr ${NETWORK}",
                             returnStatus: true
@@ -196,36 +198,50 @@ pipeline {
                 expression { params.DOCKER }
             }
             steps {
+                echo "######################## : ======> EJECUTANDO DOCKER BUILD AND RUN..."
                 script {
-                    echo "######################## : ======> EJECUTANDO DOCKER BUILD AND RUN..."
-
-                    // Obtener la versión en Windows usando un archivo temporal
-                    bat '''
-                        mvn help:evaluate -Dexpression=project.version -q -DforceStdout > version.txt
-                    '''
-                    def version = readFile('version.txt').trim()
-
-                    echo "######################## : ======> VERSIÓN RC: ${version}"
-                    echo "######################## : ======> APLICATIVO + VERSION: ${NAME_APP}:${version}"
+                    echo "=========> VERSION RC: ${NEW_VERSION}"
+                    echo "=========> APLICATIVO + VERSION: ${NAME_APP}:${NEW_VERSION}"
                     // Usar la versión capturada para los comandos Docker
 
-                    echo "=========> Eliminando contenedores con nombre: ${NAME_APP}..."
-                    bat """
-                        for /f "tokens=*" %%i in ('docker ps -q --filter "name=${NAME_APP}"') do docker stop %%i
-                        for /f "tokens=*" %%i in ('docker ps -a -q --filter "name=${NAME_APP}"') do docker rm %%i
-                    """
+                    // Verificar si existe el contenedor
+                    def containerExists = bat(
+                            script: "@docker ps -a --format '{{.Names}}' | findstr /i \"${NAME_APP}\"",
+                            returnStatus: true
+                    ) == 0
 
-                    echo "=========> Eliminando imagenes con nombre: ${NAME_APP}..."
-                    bat """
-                        for /f "tokens=*" %%i in ('docker images -q --filter "reference=${NAME_APP}*"') do docker rmi %%i
-                    """
+                    // Verificar si existe la imagen
+                    def imageExists = bat(
+                            script: "@docker images ${NAME_IMG_DOCKER} --format '{{.Repository}}' | findstr /i \"${NAME_IMG_DOCKER}\"",
+                            returnStatus: true
+                    ) == 0
+
+                    if (containerExists || imageExists) {
+                        echo "=========> Se encontraron recursos existentes, procediendo a limpiarlos..."
+
+                        if (containerExists) {
+                            echo "=========> Eliminando contenedor existente: ${NAME_APP}"
+                            bat "docker stop ${NAME_APP}"
+                            bat "docker rm ${NAME_APP}"
+                        }
+
+                        if (imageExists) {
+                            echo "=========> Eliminando imagen existente: ${NAME_IMG_DOCKER}"
+                            bat "docker rmi ${NAME_IMG_DOCKER}:${NEW_VERSION}"
+                        }
+                    } else {
+                        echo "=========> No se encontraron recursos existentes, procediendo con el despliegue..."
+                    }
+
+                    echo "=========> VERSION A DESPLEGAR: ${NEW_VERSION}"
+                    echo "=========> APLICATIVO + VERSION: ${NAME_APP}:${NEW_VERSION}"
 
                     bat """
-                        echo "=========> Construyendo nueva imagen con versión ${version}..."
-                        docker build --build-arg NAME_APP=${NAME_APP} --build-arg JAR_VERSION=${version} -t ${NAME_IMG_DOCKER}:${version} .
+                        echo "=========> Construyendo nueva imagen con versión ${NEW_VERSION}..."
+                        docker build --build-arg NAME_APP=${NAME_APP} --build-arg JAR_VERSION=${NEW_VERSION} -t ${NAME_IMG_DOCKER}:${NEW_VERSION} .
 
                         echo "=========> Desplegando el contenedor: ${NAME_APP}..."
-                        docker run -d --name ${NAME_APP} -p ${HOST_PORT}:${CONTAINER_PORT} --network=${NETWORK} --env SERVER_PORT=${HOST_PORT} ${NAME_IMG_DOCKER}:${version}
+                        docker run -d --name ${NAME_APP} -p ${HOST_PORT}:${CONTAINER_PORT} --network=${NETWORK} --env SERVER_PORT=${HOST_PORT} ${NAME_IMG_DOCKER}:${NEW_VERSION}
                     """
                 }
             }
